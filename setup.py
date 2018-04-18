d5d721c0b5be41a75b30d21e818ed11ba4359fbf
Cammy <neus2benen@gmail.com>
2018-04-18 12:09:49 +0200

    Bump version to 2.5.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 __author__ = 'notcammy'
 __email__ = 'neus2benen@gmail.com'
-__version__ = '2.5.0'
+__version__ = '2.5.1'
 
 _api_version = '1.5.5'
 _api_extensions_version = '0.3.7'
