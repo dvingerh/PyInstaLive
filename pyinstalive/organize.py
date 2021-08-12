63052ffd3ab2eb36adb364bb502751fe11221405
dvingerh <dirk.ving@gmail.com>
2021-08-12 06:43:12 +0200

    Fix and improve organize command

--- pyinstalive/organize.py
+++ pyinstalive/organize.py
@@ -14,28 +14,42 @@ except ImportError:
 def organize_files():
 
     try:
-        files = [f for f in os.listdir(pil.dl_path) if os.path.isfile(os.path.join(pil.dl_path, f)) and not f.endswith(".lock")]
-
+        files = [f for f in os.listdir(pil.dl_path) if os.path.isfile(os.path.join(pil.dl_path, f)) and not f.endswith(".lock") and (f.endswith(".mp4")) or (f.endswith(".json")) or (f.endswith(".log")) and (f != "pyinstalive.default.log")]
         not_moved = 0
         has_moved = 0
-
+        has_error = False
         username_regex = r'(?<=\d{8}_)(.*?)(?=_\d)'
+        log_username_regex = r'(pyinstalive_)(.*)'
         date_regex = r'^\d{8}'
         timestamp_regex = r'_(\d{10})_'
         raw_file_dict = {}
         new_file_dict = {}
 
         for file in files:
-            try:
-                username = re.search(username_regex, file)[0]
-                date_ts = datetime.strptime(re.search(date_regex, file)[0], '%Y%m%d').strftime('%d-%m-%Y')
-                time_ts = time.strftime('%I-%M-%S-%p', time.localtime(int(re.search(timestamp_regex, file)[1]))) 
-                file_ext = os.path.splitext(file)[1]
-                new_file = "{:s} - {:s} {:s}{:s}".format(username, date_ts, time_ts, file_ext)
-                raw_file_dict[file] = username
-                new_file_dict[file] = new_file
-            except TypeError as e:
-                logger.warn("Could not parse filename '{:s}', skipping.".format(file))
+            if file.endswith(".mp4") or file.endswith(".json"):
+                try:
+                    username = re.search(username_regex, file)[0]
+                    date_ts = datetime.strptime(re.search(date_regex, file)[0], '%Y%m%d').strftime('%d-%m-%Y')
+                    time_ts = time.strftime('%I-%M-%S-%p', time.localtime(int(re.search(timestamp_regex, file)[1]))) 
+                    file_ext = os.path.splitext(file)[1]
+                    new_file = "{:s} - {:s} {:s}{:s}".format(username, date_ts, time_ts, file_ext)
+                    raw_file_dict[file] = username
+                    new_file_dict[file] = new_file
+                except TypeError as e:
+                    logger.warn("Could not parse filename '{:s}', skipping.".format(file))
+                    not_moved += 1 
+                    has_error = True
+            elif file.endswith(".log"):
+                try:
+                    username = re.search(log_username_regex, file)[2]
+                    username_fix = os.path.splitext(username)[0]
+                    new_file = "logs-{:s}.log".format(username_fix)
+                    raw_file_dict[file] = username_fix
+                    new_file_dict[file] = new_file
+                except TypeError as e:
+                    logger.warn("Could not parse filename '{:s}', skipping.".format(file))
+                    not_moved += 1 
+                    has_error = True
         
         for filename, username in raw_file_dict.items():
             try:
@@ -56,10 +70,14 @@ def organize_files():
                 logger.binfo("Did not move and rename '{:s}' because it already exists.".format(filename))
                 not_moved += 1
 
-        logger.separator()
-        logger.info("{} {} moved.".format(has_moved, "file was" if has_moved == 1 else "files were"))
+        if (has_moved > 0) or (not_moved > 0 and has_error):
+            logger.separator()
+        if has_moved:
+            logger.info("{} {} moved.".format(has_moved, "file was" if has_moved == 1 else "files were"))
         if not_moved:
             logger.binfo("{} {} not moved.".format(not_moved, "file was" if not_moved == 1 else "files were"))
+        if len(files) == 0:
+            logger.warn("Could not find any files to organize.")
         logger.separator()
     except Exception as e:
         logger.error("Could not organize files: {:s}".format(str(e)))
