5c3856ea16ee602b962688929b61c9ac035f4969
dvingerh <dirk.ving@gmail.com>
2022-06-17 09:52:38 +0200

    Add timeout limits to api requests

--- pyinstalive/api.py
+++ pyinstalive/api.py
@@ -17,33 +17,33 @@ def do_login():
     "queryParams": {},
     "optIntoOneTap": "false"
     }
-    response = globals.session.session.post(Constants.LOGIN_AJAX, data=login_data)
+    response = globals.session.session.post(Constants.LOGIN_AJAX, data=login_data, timeout=5)
     return json.loads(response.text)
 
 def get_login_state():
-    response = globals.session.session.get(Constants.BASE_WEB)
+    response = globals.session.session.get(Constants.BASE_WEB, timeout=5)
     return helpers.get_shared_data(response.text)
 
 def get_user_info():
-    response = globals.session.session.get(Constants.USER_INFO.format(globals.download.download_user))
+    response = globals.session.session.get(Constants.USER_INFO.format(globals.download.download_user), timeout=5)
     return json.loads(response.text) if response.status_code == 200 else {}
 
 def get_reels_tray():
-    response = globals.session.session.get(Constants.REELS_TRAY)
+    response = globals.session.session.get(Constants.REELS_TRAY, timeout=5)
     return json.loads(response.text)
 
 def get_single_live():
-    response = globals.session.session.get(Constants.LIVE_STATE_USER.format(globals.download.download_user_id))
+    response = globals.session.session.get(Constants.LIVE_STATE_USER.format(globals.download.download_user_id), timeout=5)
     return json.loads(response.text)
 
 def get_comments():
-    response = globals.session.session.get(Constants.LIVE_COMMENT.format(globals.download.livestream_object_init.get('id'), str(globals.comments.comments_last_ts)))
+    response = globals.session.session.get(Constants.LIVE_COMMENT.format(globals.download.livestream_object_init.get('id'), str(globals.comments.comments_last_ts)), timeout=5)
     return json.loads(response.text)
 
 def get_stream_data():
-    response = globals.session.session.get(Constants.LIVE_STATE_USER.format(globals.download.download_user_id))
+    response = globals.session.session.get(Constants.LIVE_STATE_USER.format(globals.download.download_user_id), timeout=5)
     return json.loads(response.text)
 
 def do_heartbeat():
-    response = globals.session.session.post(Constants.LIVE_HEARTBEAT.format(globals.download.livestream_object_init.get('id')))
+    response = globals.session.session.post(Constants.LIVE_HEARTBEAT.format(globals.download.livestream_object_init.get('id')), timeout=5)
     return json.loads(response.text)
